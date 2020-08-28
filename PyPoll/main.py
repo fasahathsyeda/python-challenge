183b36df708584c77af3299aeb0b4453a3353a08
fasahath <syedafasahath@gmail.com>
2020-08-27 23:28:48 -0400

    changes made to main of pypoll data

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -17,15 +17,12 @@ with open(inputfile, newline='') as myCsvFile:
 	    num_votes.append(row[0])
 	    candidate.append(row[2])
     representatives=list(set(candidate))
-    #print(representatives)
-    print("Election Results")
-    print("--------------------------------------")
-    print(f"Total Votes : {len(num_votes)}")
-    print("--------------------------------------")
+   
+    
 
     for i in representatives:
         representativevotes=0
-        #print(i)
+        
         for j in range(len(num_votes)):
             if(candidate[j]==i):
                 representativevotes=representativevotes+1
@@ -40,6 +37,12 @@ with open(output_path,"w",newline = '') as output_file:
     output_file.write("Election Results\n")
     output_file.write("-------------------------------------\n")
     output_file.write(f"Total Votes : {len(num_votes)}\n")
+    output_file.write("--------------------------------------\n")
+    print("Election Results")
+    print("--------------------------------------")
+    print(f"Total Votes : {len(num_votes)}")
+    print("--------------------------------------")
+
     for k in range(len(representatives)):
         print(f"{representatives[k]} : {individualVotes[k]/len(num_votes):.3%}%   ({individualVotes[k]})")
         output_file.write(f"{representatives[k]} : {individualVotes[k]/len(num_votes):.3%}%   ({individualVotes[k]})\n")
