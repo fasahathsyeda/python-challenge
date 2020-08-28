6d78016023bea5a5d13cd551447db7cfb3c3916a
fasahath <syedafasahath@gmail.com>
2020-08-27 23:18:31 -0400

    changing pyparagraph

--- Bonus/PyParagraph/main.py
+++ Bonus/PyParagraph/main.py
@@ -1,10 +1,24 @@
 import os
 import csv
+import re
 #inputfile=os.path.join('raw_data','paragraph_1.txt')
 output_path = os.path.join('paragraph_1_analysis.txt')
 
 file1=open("raw_data/paragraph_1.txt",'r')
 para_1=file1.read()
-print(para_1)
-word_count=para_1.split()
-print("Appoximate word count : " + str(len(word_count)))
+#print(para_1)
+letters=0
+for i in para_1:
+    if i.isalpha():
+        letters=letters+1
+
+word=para_1.split()
+print("Paragraph Analysis")
+print("------------------------------")
+print("Appoximate word count : " + str(len(word)))
+sentence=re.split("(?<=[.!?]) +",para_1)
+print("Approximate sentence count : " + str(len(sentence)))
+lettercount=letters/len(word)
+print(f"Average Letter Count:  {lettercount:.3}")
+print(f"Average Sentence Length : {len(word)/len(sentence)}")
+
