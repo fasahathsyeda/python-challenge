6279d5bbd561b4ad3edf1651a310a3316771e9c1
fasahath <syedafasahath@gmail.com>
2020-08-28 21:48:08 -0400

    bonus challenge

--- Bonus/PyParagraph/main.py
+++ Bonus/PyParagraph/main.py
@@ -12,12 +12,18 @@ def Para_analysis(inputfile,outputfile):
     for i in para_1:
         if i.isalpha():
             letters=letters+1
-    word=para_1.split()
+    word=para_1.split(sep=" ")
     print("Paragraph Analysis")
     print("------------------------------")
     print("Appoximate word count : " + str(len(word)))
+    
+    a="\n\n"
+    sen=para_1.split("\n\n")
     sentence=re.split("(?<=[.!?]) +",para_1)
+    
+    #print(sentence[2])
     print("Approximate sentence count : " + str(len(sentence)))
+    #print(sentence)
     lettercount=letters/len(word)
     print(f"Average Letter Count:  {lettercount:.3}")
     print(f"Average Sentence Length : {len(word)/len(sentence)}")
@@ -29,5 +35,5 @@ def Para_analysis(inputfile,outputfile):
     file2.write("Approximate sentence count : " + str(len(sentence))+"\n")
     file2.write(f"Average Letter Count:  {lettercount:.3}\n")
     file2.write(f"Average Sentence Length : {len(word)/len(sentence)}\n")
-Para_analysis("raw_data/paragraph_1.txt","paragraph_1_analysis.txt")
-
+#Para_analysis("raw_data/paragraph_1.txt","paragraph_1_analysis.txt")
+Para_analysis("raw_data/paragraph_2.txt","paragraph_2_analysis.txt")
